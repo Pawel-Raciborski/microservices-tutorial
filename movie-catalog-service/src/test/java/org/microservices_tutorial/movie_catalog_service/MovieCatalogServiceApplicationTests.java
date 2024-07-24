fe7d95664bdbe00427bc4e889577e98f7194f56c
Pawel-Raciborski <wojak2845@gmail.com>
2024-07-24 20:00:41 +0200

    Project init

---
+++ movie-catalog-service/src/test/java/org/microservices_tutorial/movie_catalog_service/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microservices_tutorial.movie_catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
