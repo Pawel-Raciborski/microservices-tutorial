9e413040b1809f160666800b04b3442c220d1d0a
Pawel-Raciborski <wojak2845@gmail.com>
2024-07-24 20:16:16 +0200

    MovieResource rest controller created

--- movie-catalog-service/src/main/java/org/microservices_tutorial/movie_catalog_service/models/CategoryItem.java
+++ movie-catalog-service/src/main/java/org/microservices_tutorial/movie_catalog_service/models/CategoryItem.java
@@ -1,2 +1,37 @@
-package org.microservices_tutorial.movie_catalog_service.models;public class CategoryItem {
+package org.microservices_tutorial.movie_catalog_service.models;
+
+public class CategoryItem {
+    private String name;
+    private String description;
+    private int rating;
+
+    public CategoryItem(String name, String description, int rating) {
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
--- movie-catalog-service/src/main/java/org/microservices_tutorial/movie_catalog_service/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/org/microservices_tutorial/movie_catalog_service/resources/MovieCatalogResource.java
@@ -1,2 +1,21 @@
-package org.microservices_tutorial.movie_catalog_service.resources;public class MovieCatalogResource {
+package org.microservices_tutorial.movie_catalog_service.resources;
+
+import org.microservices_tutorial.movie_catalog_service.models.CategoryItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CategoryItem> getCatalog(@PathVariable("userId") String userId){
+        return Collections.singletonList(
+                new CategoryItem("Harry-Potter","Some description", 4)
+        );
+    }
 }
